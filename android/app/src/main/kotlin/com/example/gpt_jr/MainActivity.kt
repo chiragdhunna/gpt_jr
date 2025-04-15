e6e7737ff579ada6bc8bf9bc7bb297775eb1b8a0
chiragdhunna <chiragdhunna2468@gmail.com>
2025-04-15 22:54:06 +0530

    chore: migrate Android build scripts to Kotlin DSL and add Linux runner
    Migrate Android build scripts from Groovy to Kotlin DSL for better maintainability and consistency. Add Linux runner files to support Flutter desktop applications. Update Gradle properties and dependencies to align with the latest Flutter stable release.

--- android/app/src/main/kotlin/com/example/gpt_jr/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/gpt_jr/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.gpt_jr
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,10 @@ import Foundation
 
 import firebase_core
 import flutter_tts
-import speech_to_text_macos
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
